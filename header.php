3b0c88495beea7a707de43c0230589064c0c83d6
alfonsojk <alfonso.juarezkelly@gmail.com>
2020-11-10 19:36:26 -0600

    first commit

---
+++ header.php
@@ -0,0 +1,111 @@
+
+<html>
+	<head>
+<!-- Main -->
+<div id="main">
+
+<!-- One -->
+    <section id="one">
+        <header class="major">
+            <h1 style="justify-content: center;display: flex;font-size: 60;">
+                Web Developer
+            </h1>
+        </header>
+        <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
+    </section>
+
+<!-- Two -->
+    <section id="two">
+        <h2>Recent Work</h2>
+        <div class="row">
+            <article class="col-6 col-12-xsmall work-item">
+                <a href="images/fulls/01.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/01.jpg" alt="" /></a>
+                <div class="centered" style="display: grid; justify-content: center;">
+                <h3>Colectivo Lateral de Arquitectura</h3>
+                <p style="
+    display: fr;
+    display: grid;
+    justify-content: center;
+">Colateral's Official Website </p>
+            </div>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+                <a href="images/fulls/02.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/02.jpg" alt="" /></a>
+                <div class="centered">
+                <h3>5 Elementos</h3>
+                <p>Land Sale Website located in Monterrey</p>
+                </div>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+                <a href="images/fulls/03.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/03.jpg" alt="" /></a>
+                <div class="centered">
+                <h3>Social Media App</h3>
+                <p>App Builded in MERNG Stack.</p>
+                </div>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+                <a href="images/fulls/04.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/04.jpg" alt="" /></a>
+                <div class="centered">
+                <h3>Tetris App</h3>
+                <p>App builded in Javascript</p>
+                </div>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+                <a href="images/fulls/05.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/05.jpg" alt="" /></a>
+                <div class="centered">
+                <h3>Flappy Bird App</h3>
+                <p>App builded in Javascript</p>
+                </div>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+                <a href="images/fulls/06.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/06.jpg" alt="" /></a>
+                <div class="centered">
+                <h3>Miniseries</h3>
+                <p>Pre-Loader Video in HTML</p>
+                </div>
+            </article>
+        </div>
+        <ul class="actions">
+            <li><a href="#" class="button">Full Portfolio</a></li>
+        </ul>
+    </section>
+
+<!-- Three -->
+    <section id="three">
+        <h2>Get In Touch</h2>
+        <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
+        <div class="row">
+            <div class="col-8 col-12-small">
+                <form method="post" action="#">
+                    <div class="row gtr-uniform gtr-50">
+                        <div class="col-6 col-12-xsmall"><input type="text" name="name" id="name" placeholder="Name" /></div>
+                        <div class="col-6 col-12-xsmall"><input type="email" name="email" id="email" placeholder="Email" /></div>
+                        <div class="col-12"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
+                    </div>
+                </form>
+                <ul class="actions">
+                    <li><input type="submit" value="Send Message" /></li>
+                </ul>
+            </div>
+            <div class="col-4 col-12-small">
+                <ul class="labeled-icons">
+                    <li>
+                        <h3 class="icon solid fa-home"><span class="label">Address</span></h3>
+                        1234 Somewhere Rd.<br />
+                        Nashville, TN 00000<br />
+                        United States
+                    </li>
+                    <li>
+                        <h3 class="icon solid fa-mobile-alt"><span class="label">Phone</span></h3>
+                        000-000-0000
+                    </li>
+                    <li>
+                        <h3 class="icon solid fa-envelope"><span class="label">Email</span></h3>
+                        <a href="#">hello@untitled.tld</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</div>
+
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+		<title>Strata by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="#" class="image avatar"><img src="<?php echo get_stylesheet_directory_uri()?>/images/avatar.jpg" alt="" /></a>
+					<h1>
+						<strong style="font-size: 40;display: flex;justify-content: center;">
+							Alfonso JK
+						</strong>
+					</h1>
+				</div>
+			</header>
+<?php get_footer(); ?>
+
